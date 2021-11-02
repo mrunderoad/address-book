2b65b12228bed2bec81a061147d653f9cde3f901
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-02 10:20:46 -0700

    Add button and function for adding work/personal addresses
    co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -29,11 +29,19 @@ AddressBook.prototype.deleteContact = function(id) {
   return true;
 };
 
+function Address(address, work, email) {
+  this.address = address;
+  this.work = work;
+  this.email = email;
+}
+
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, email, address) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.email = email;  
+  this.address = address;
 }
 
 Contact.prototype.fullName = function() {
@@ -59,6 +67,10 @@ function showContact(contactId) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
+  $(".email-address").html(contact.email);
+  $(".address").html(contact.address)
+
+  
   let buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
@@ -82,11 +94,25 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var inputtedEmail = $("input#new-email").val();
+    var inputtedAddress = $("input#new-address").val();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    $("input#new-email").val("");
+    $("input#new-address").val("");
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedAddress);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
+  });
+  $("form#new-address").submit(function(event) {
+    event.preventDefault();
+    var inputtedAddress = $("input#address").val();
+    var inputtedWork = $("input#work").val();
+    var inputtedEmail = $("input#email").val();
+    $("input#address").val("");
+    $("input#work").val("");
+    $("input#email").val("");
+
   });
 });
